7675b57f1dcdefe56f29872c98db104b7033b535
kyletam27 <kyle_tam1@hotmail.com>
2016-09-05 04:29:45 +0000

    DB connection + php file skeletion

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
-   echo "hello world";
+   
+   
+   //-------------------------------------------- Database Connection ------------------------------------
+   
+    //credentials for localserver
+    $servername = getenv('IP');
+    $username = "tamkylet";
+    $password = "password";
+    $database = "dine";
+    $dbport = 3306;
+    
+    // Create connection
+    $db = new mysqli($servername, $username, $password, $database, $dbport);
+
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    } 
+    echo "Connected successfully (".$db->host_info.")";
+    
+    echo "<br>";
+    
+    //-------------------------------------------- Database Connection ------------------------------------
+    
+   
+   
+   
+    //-------------------------------------------- Registration -------------------------------------------   
+   
+    
+   
+   
+    //-------------------------------------------- Registration -------------------------------------------    
+   
+   
 ?>
