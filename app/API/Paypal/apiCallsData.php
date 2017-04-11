67a4fbcb969340d6d12fd2afc2552642985d9c87
kyletam27 <kyle_tam1@hotmail.com>
2017-04-11 04:50:33 +0000

    Paypal API progress2 - minor link stub fixes

--- app/API/Paypal/apiCallsData.php
+++ app/API/Paypal/apiCallsData.php
@@ -11,11 +11,10 @@ if (session_id() !== "") {
     * $_SESSION['expressCheckoutPaymentData'] is used in the Express Checkout flow
     * $_SESSION['markFlowPaymentData'] is used for the Proceed to Checkout/Mark flow
     */
-$hostName = $_SERVER['HTTP_HOST'];
-$appName = explode("/", $_SERVER['REQUEST_URI'])[1];
-$cancelUrl= "http://".$hostName."/".$appName."/cancel.php";
-$payUrl = "http://".$hostName."/".$appName."/pay.php";
-$placeOrderUrl = "http://".$hostName."/".$appName."/placeOrder.php";
+
+$cancelUrl= "https://php-projects-kyletam27.c9users.io/DineRoulette-tamkylet/app/API/Paypal/cancel.php";
+$payUrl = "https://php-projects-kyletam27.c9users.io/DineRoulette-tamkylet/app/API/Paypal/pay.php";
+$placeOrderUrl = "https://php-projects-kyletam27.c9users.io/DineRoulette-tamkylet/app/API/Paypal/placeOrder.php";
 
 $_SESSION['expressCheckoutPaymentData'] = '{
                                   "transactions":[
