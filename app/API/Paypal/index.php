ab6087b58cf87caebb3d14f989684eb398bb54cf
kyletam27 <kyle_tam1@hotmail.com>
2017-05-02 00:29:12 +0000

    Push to API branch

--- app/API/Paypal/index.php
+++ app/API/Paypal/index.php
@@ -3,9 +3,9 @@
     include('apiCallsData.php');
     include('header.php');
     include('paypalConfig.php');
-    
+
     session_start();
-    
+
     //credentials for localserver
     $servername = getenv('IP');
     $username = "tamkylet";
@@ -13,47 +13,47 @@
     $database = "dine";
     $dbport = 3306;
     session_start();
-    
+
     // Create connection
     $db = new mysqli($servername, $username, $password, $database, $dbport);
-    
+
     // Check connection
     if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
-    } 
+    }
 
     $userSignedIn = $_SESSION['username'];
 
     $query1 = "SELECT *
                FROM user
                WHERE username = '$userSignedIn'";
-              
+
     $result1 = $db->query($query1);
-        if (!$result1) die ("Database access failed." . $db->error);  
-    
+        if (!$result1) die ("Database access failed." . $db->error);
+
     $row = $result1->fetch_assoc();
-    
+
     $inviterName = $row["invitername"];
     $suggestRest = $row["suggestedrestaurant"];
-    
+
     $_SESSION['inviterName'] = $inviterName;
     $_SESSION['suggestedrestaurant'] = $suggestRest;
 
     $query2 = "SELECT *
                FROM user
                WHERE username = '$inviterName'";
-              
+
     $result2 = $db->query($query2);
-        if (!$result1) die ("Database access failed." . $db->error);  
-    
+        if (!$result1) die ("Database access failed." . $db->error);
+
     $row1 = $result2->fetch_assoc();
-    
+
     $inviterEmail = $row1["email"];
-    
+
     $_SESSION['inviterEmail'] = $inviterEmail;
 
     $xmldata = simplexml_load_file('/home/ubuntu/workspace/DineRoulette-tamkylet/app/API/GoogleMaps/restaurantList.xml');
-  
+
     foreach ($xmldata-> resturant as $resturant)
     {
         if($resturant->name == $suggestRest)
@@ -65,7 +65,7 @@
     }
 
     include ("/home/ubuntu/workspace/DineRoulette-tamkylet/app/html/skeletontop.html");
-    
+
     //setting the environment for Checkout script
     if(SANDBOX_FLAG) {
         $environment = SANDBOX_ENV;
@@ -74,17 +74,17 @@
     }
 
     echo <<<_END
-    
+
    <body>
-   
+
             <div class="inbordermid">
-            
+
             <div class="inbordermidPAD">
             <br/>
-            
+
                 <p class="titles">Dine meetup</p>
                 <blockquote class="resturantNames">$suggestRest</blockquote>
-                <blockquote><img src="/DineRoulette-tamkylet/app/images/$suggestImg.png" width="100%"/></blockquote> 
+                <blockquote><img src="/DineRoulette-tamkylet/app/images/$suggestImg.png" width="100%"/></blockquote>
 _END;
 
     echo "<blockquote>";
@@ -92,25 +92,24 @@ _END;
     echo "</blockquote>";
     echo "<blockquote>$suggestAddress</blockquote>";
 ?>
-
             </br>
             <h3>Agreement to Dine</h3>
 
             <blockquote>
-                 Agree to policies! sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                 Agree to policies! sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
                  officia deserunt mollit anim id est laborum.
             </blockquote>
-            
+
             <h3>Inviter</h3>
-            
+
             <blockquote>
                 <img src='/DineRoulette-tamkylet/app/images/memberPictures/<?php echo "$inviterName" ?>.png' style='width:5.25em;height:5.25em' alt='[]' />
                 <p><?php echo "$inviterName" ?></p>
             </blockquote>
-            
+
 
             <h3> Pay Restuarant </h3>
             <form id="paypalMid" action="startPayment.php" method="POST">
@@ -145,14 +144,14 @@ _END;
      };
      </script>
      <script src="//www.paypalobjects.com/api/checkout.js" async></script>
-     
+
      <h3>Reject this date</h3>
-     
+
      <form class="requesting" method="post" action="/DineRoulette-tamkylet/app/php/home.php">
         <input type="hidden" name="rejectDate" value="reject">
         <button input type="submit" value="Reject" id="reject">Reject</button>
-    </form>  
-     
+    </form>
+
 <?php
 
 
@@ -163,9 +162,9 @@ _END;
 
           <div class="inborderbottom">
           </div>
-            
+
         </div>
-        
+
    </body>
-    
-</html>    
+
+</html>
